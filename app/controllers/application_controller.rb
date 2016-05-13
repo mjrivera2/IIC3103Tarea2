28e14d7c5125bcb0bff57618c3807b31ea851e7a
Jesu <mjrivera2@puc.cl>
2016-05-13 18:30:17 -0300

    connect instagram api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ class ApplicationController < ActionController::Base
   respond_to :json
 
 
+def buscar
+  @tags = HTTParty.get('https://api.instagram.com/v1/tags/'+ params[:tag] +'/media/recent?access_token=9994244.1fb234f.8893dfbb48d840aab8ea4b3e92be20e7')
+  if(@tags != nil)
+    render status: 200, json: @tags["data"]
+  else
+    render status: 404
+  end
+end
+
 
   #Metodo para retornar Json
   def self.respond_to(*mimes)
